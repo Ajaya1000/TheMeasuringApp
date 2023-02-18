06577a568ad31226fcfcf06c0fd2abf0494439d3
Ajaya Mati <ajaya.mati@nobroker.in>
2023-02-18 11:23:41 +0530

    configure initial AR scene view

--- WallPaintingApp/ViewController.swift
+++ WallPaintingApp/ViewController.swift
@@ -6,15 +6,38 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // setup scene
+        setupScene()
     }
-
-
+    
+    private func setupScene() {
+        
+        // set delegate ARSCNViewDelegate
+        sceneView.delegate = self
+        
+        // showing statitics (fps, timing info)
+        sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
+        
+        // debug points
+        sceneView.debugOptions = [.showFeaturePoints, .showWorldOrigin]
+        
+        // create new scene
+        let scene = SCNScene()
+        sceneView.scene = scene
+    }
 }
 
+extension ViewController: ARSCNViewDelegate {
+    
+}
